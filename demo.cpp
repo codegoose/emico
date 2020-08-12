8a5e6a1571ee63db6d8effd54a861c80dbe18d17
Brandon White <b.white.1@pm.me>
2020-08-12 12:21:36 -0700

    Usage demo.

---
+++ demo.cpp
@@ -0,0 +1,17 @@
+#include <emico.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+
+	cout << "There are " << emico::assets.size() << " embedded assets." << endl;
+
+	for (auto &asset : emico::assets) {
+		
+		cout << "\t" << asset.first << // Unique ID based on the original file path.
+			" : " << asset.second.first << // The memory address of the file contents.
+			" (" << asset.second.second << " bytes)" << endl; // Length in bytes of the content.
+	}
+}
